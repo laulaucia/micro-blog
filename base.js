ad6488f0108c0384d16371cd8a74daafee6bc6df
Laulaucia <buitrago.laura@gmail.com>
2015-10-12 19:35:24 -0700

    added comments of what needs to be done

--- base.js
+++ base.js
@@ -2,6 +2,10 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  // code in here
+//post input area
+//new post container
+//adding a new post div aka submission
+//deleting posts
+
 
 });
