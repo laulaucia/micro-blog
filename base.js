55423d4ec178b1cdfd6b42d9c7503c1163e4d1fc
Laulaucia <buitrago.laura@gmail.com>
2015-10-12 20:14:44 -0700

    added a text box and a submit button

--- base.js
+++ base.js
@@ -3,6 +3,11 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
 
 //post input area
+var post = new Object({
+	title: title,
+	
+});
+
 //new post container "posts" area
 //adding a new post div aka submission
 //deleting posts
