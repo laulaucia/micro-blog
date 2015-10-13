08c66c2c2a35a90cdf336853e1352f826b1d8e07
Laulaucia <buitrago.laura@gmail.com>
2015-10-12 22:36:45 -0700

    things are starting to work

--- base.js
+++ base.js
@@ -4,18 +4,31 @@ $(document).ready(function(){
 
 //post input area
 var post = new Object({
-	title: title,
-	
+
 });
 
+var deleteButton = '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>';
+
 //new post container "posts" area
-//adding a new post div aka submission
+//adding a new post div aka submission but first checking if the form is empty
+  $('#new-blog-post').submit(function (e) {
+    e.preventDefault();
+  if($('#post-content').val().length>0){
+  	var postContent = $("#post-content").val();
+  	var newPost = '<li class="list-group-item alert">'+postContent+ deleteButton+'</li>';
+  	$('#posts').prepend(newPost);
+  }
 //deleting posts
+
+$('.close').on('click', function(){
+	$('.list-group-item').html.empty;
+	console.log(this);
+});
 
 
 //STRETCH goals
 // add a counter for each post
 //using local storage
-
+});
 
 });
